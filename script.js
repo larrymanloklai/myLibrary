cc050fffd31aa994ebca0bd90b61e9d2996d66b4
larrymanloklai <larrymanlok.lai@gmail.com>
2024-08-31 21:21:10 +1000

    finished basic template html, css, js

---
+++ script.js
@@ -0,0 +1,18 @@
+.container-wrap {
+    display: flex;
+    flex-direction: column; /* Stack children vertically */
+    justify-content: center; /* Center children vertically */
+    align-items: center; /* Center children horizontally */
+}
+
+.header-wrap {
+    display: flex;
+    justify-content: space-between; /* Distribute items with space between them */
+    align-items: center; /* Align items in the center of the cross axis */
+}
+
+.body-wrap {
+    display: flex;
+    flex-direction: column; /* Stack children vertically */
+    align-items: flex-start; /* Align items to the start of the cross axis */
+}
